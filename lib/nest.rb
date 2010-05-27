f01298d35e5fa81fa75bc6b5bbe17a5c740d5b9a
Michel Martens <michel@soveran.com>
2010-05-26 22:27:31 -0300

    Move available commands to the METHODS constants.

--- lib/nest.rb
+++ lib/nest.rb
@@ -1,5 +1,16 @@
 class Nest < String
   VERSION = "0.0.4"
+  METHODS = [:append, :blpop, :brpop, :decr, :decrby, :del, :exists,
+  :expire, :expireat, :get, :getset, :hdel, :hexists, :hget, :hgetall,
+  :hincrby, :hkeys, :hlen, :hmset, :hset, :hvals, :incr, :incrby,
+  :lindex, :llen, :lpop, :lpush, :lrange, :lrem, :lset, :ltrim, :move,
+  :rename, :renamenx, :rpop, :rpoplpush, :rpush, :sadd, :scard, :sdiff,
+  :sdiffstore, :set, :setex, :setnx, :sinter, :sinterstore, :sismember,
+  :smembers, :smove, :sort, :spop, :srandmember, :srem, :substr,
+  :sunion, :sunionstore, :ttl, :type, :unwatch, :watch, :zadd, :zcard,
+  :zincrby, :zinterstore, :zrange, :zrangebyscore, :zrank, :zrem,
+  :zremrangebyrank, :zremrangebyscore, :zrevrange, :zrevrank, :zscore,
+  :zunionstore]
 
   def initialize(key, redis = nil)
     super(key)
@@ -10,17 +21,7 @@ class Nest < String
     self.class.new("#{self}:#{key}", redis)
   end
 
-  [:append, :blpop, :brpop, :decr, :decrby, :del, :exists, :expire,
-  :expireat, :get, :getset, :hdel, :hexists, :hget, :hgetall,
-  :hincrby, :hkeys, :hlen, :hmset, :hset, :hvals, :incr, :incrby,
-  :lindex, :llen, :lpop, :lpush, :lrange, :lrem, :lset, :ltrim, :move,
-  :rename, :renamenx, :rpop, :rpoplpush, :rpush, :sadd, :scard,
-  :sdiff, :sdiffstore, :set, :setex, :setnx, :sinter, :sinterstore,
-  :sismember, :smembers, :smove, :sort, :spop, :srandmember, :srem,
-  :substr, :sunion, :sunionstore, :ttl, :type, :zadd, :zcard,
-  :zincrby, :zinterstore, :zrange, :zrangebyscore, :zrank, :zrem,
-  :zremrangebyrank, :zremrangebyscore, :zrevrange, :zrevrank, :zscore,
-  :zunionstore].each do |meth|
+  METHODS.each do |meth|
     define_method(meth) do |*args|
       redis.send(meth, self, *args)
     end
