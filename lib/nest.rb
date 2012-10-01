a4aee944d081ceb2f916b5b806ab812f4c0ad01a
Michel Martens <michel@soveran.com>
2012-10-01 09:12:50 -0300

    Use version from gemspec.

--- lib/nest.rb
+++ lib/nest.rb
@@ -1,8 +1,6 @@
 require "redis"
 
 class Nest < String
-  VERSION = "1.1.1"
-
   METHODS = [:append, :bitcount, :blpop, :brpop, :brpoplpush, :decr,
   :decrby, :del, :dump, :exists, :expire, :expireat, :get, :getbit,
   :getrange, :getset, :hdel, :hexists, :hget, :hgetall, :hincrby,
