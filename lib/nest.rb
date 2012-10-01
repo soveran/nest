4f2240a36d38c3f2ab3fb1bd18338ce06f318cbf
Michel Martens <michel@soveran.com>
2012-10-01 09:08:33 -0300

    Update commands.

--- lib/nest.rb
+++ lib/nest.rb
@@ -3,19 +3,21 @@ require "redis"
 class Nest < String
   VERSION = "1.1.0"
 
-  METHODS = [:append, :blpop, :brpop, :brpoplpush, :decr, :decrby,
-  :del, :exists, :expire, :expireat, :get, :getbit, :getrange, :getset,
-  :hdel, :hexists, :hget, :hgetall, :hincrby, :hkeys, :hlen, :hmget,
-  :hmset, :hset, :hsetnx, :hvals, :incr, :incrby, :lindex, :linsert,
-  :llen, :lpop, :lpush, :lpushx, :lrange, :lrem, :lset, :ltrim, :move,
-  :persist, :publish, :rename, :renamenx, :rpop, :rpoplpush, :rpush,
-  :rpushx, :sadd, :scard, :sdiff, :sdiffstore, :set, :setbit, :setex,
-  :setnx, :setrange, :sinter, :sinterstore, :sismember, :smembers,
-  :smove, :sort, :spop, :srandmember, :srem, :strlen, :subscribe,
-  :sunion, :sunionstore, :ttl, :type, :unsubscribe, :watch, :zadd,
-  :zcard, :zcount, :zincrby, :zinterstore, :zrange, :zrangebyscore,
-  :zrank, :zrem, :zremrangebyrank, :zremrangebyscore, :zrevrange,
-  :zrevrangebyscore, :zrevrank, :zscore, :zunionstore]
+  METHODS = [:append, :bitcount, :blpop, :brpop, :brpoplpush, :decr,
+  :decrby, :del, :dump, :exists, :expire, :expireat, :get, :getbit,
+  :getrange, :getset, :hdel, :hexists, :hget, :hgetall, :hincrby,
+  :hincrbyfloat, :hkeys, :hlen, :hmget, :hmset, :hset, :hsetnx, :hvals,
+  :incr, :incrby, :incrbyfloat, :lindex, :linsert, :llen, :lpop,
+  :lpush, :lpushx, :lrange, :lrem, :lset, :ltrim, :move, :persist,
+  :pexpire, :pexpireat, :psetex, :pttl, :publish, :rename, :renamenx,
+  :restore, :rpop, :rpoplpush, :rpush, :rpushx, :sadd, :scard,
+  :sdiff, :sdiffstore, :set, :setbit, :setex, :setnx, :setrange,
+  :sinter, :sinterstore, :sismember, :smembers, :smove, :sort, :spop,
+  :srandmember, :srem, :strlen, :subscribe, :sunion, :sunionstore,
+  :ttl, :type, :unsubscribe, :watch, :zadd, :zcard, :zcount,
+  :zincrby, :zinterstore, :zrange, :zrangebyscore, :zrank, :zrem,
+  :zremrangebyrank, :zremrangebyscore, :zrevrange, :zrevrangebyscore,
+  :zrevrank, :zscore, :zunionstore]
 
   attr :redis
 
