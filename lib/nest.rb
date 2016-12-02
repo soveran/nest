063b18455d76ca76b9a4273023d26c343bfb0756
Michel Martens <michel@soveran.com>
2016-12-02 12:55:37 +0100

    Update methods

--- lib/nest.rb
+++ lib/nest.rb
@@ -1,23 +1,25 @@
 require "redic"
 
 class Nest < String
-  METHODS = [:append, :bitcount, :bitpos, :blpop, :brpop, :brpoplpush,
-    :decr, :decrby, :del, :dump, :exists, :expire, :expireat, :get,
-    :getbit, :getrange, :getset, :hdel, :hexists, :hget, :hgetall,
-    :hincrby, :hincrbyfloat, :hkeys, :hlen, :hmget, :hmset, :hset,
-    :hsetnx, :hstrlen, :hvals, :incr, :incrby, :incrbyfloat, :lindex,
-    :linsert, :llen, :lpop, :lpush, :lpushx, :lrange, :lrem, :lset,
-    :ltrim, :move, :persist, :pexpire, :pexpireat, :pfadd, :pfcount,
-    :pfmerge, :psetex, :pttl, :publish, :rename, :renamenx, :restore,
-    :rpop, :rpoplpush, :rpush, :rpushx, :sadd, :scard, :sdiff, :sdiffstore,
-    :set, :setbit, :setex, :setnx, :setrange, :sinter, :sinterstore,
-    :sismember, :smembers, :smove, :sort, :spop, :srandmember, :srem,
-    :strlen, :subscribe, :sunion, :sunionstore, :ttl, :type, :unsubscribe,
-    :watch, :zadd, :zcard, :zcount, :zincrby, :zinterstore, :zlexcount,
-    :zrange, :zrangebylex, :zrevrangebylex, :zrangebyscore, :zrank,
-    :zrem, :zremrangebylex, :zremrangebyrank, :zremrangebyscore,
-    :zrevrange, :zrevrangebyscore, :zrevrank, :zscore, :zunionstore,
-    :sscan, :hscan, :zscan]
+  METHODS = [:append, :bitcount, :bitfield, :bitpos, :blpop, :brpop,
+    :brpoplpush, :decr, :decrby, :del, :dump, :exists, :expire,
+    :expireat, :geoadd, :geohash, :geopos, :geodist, :georadius,
+    :georadiusbymember, :get, :getbit, :getrange, :getset, :hdel,
+    :hexists, :hget, :hgetall, :hincrby, :hincrbyfloat, :hkeys, :hlen,
+    :hmget, :hmset, :hset, :hsetnx, :hstrlen, :hvals, :incr, :incrby,
+    :incrbyfloat, :lindex, :linsert, :llen, :lpop, :lpush, :lpushx,
+    :lrange, :lrem, :lset, :ltrim, :move, :persist, :pexpire, :pexpireat,
+    :pfadd, :pfcount, :pfmerge, :psetex, :pttl, :publish, :rename,
+    :renamenx, :restore, :rpop, :rpoplpush, :rpush, :rpushx, :sadd,
+    :scard, :sdiff, :sdiffstore, :set, :setbit, :setex, :setnx,
+    :setrange, :sinter, :sinterstore, :sismember, :smembers, :smove,
+    :sort, :spop, :srandmember, :srem, :strlen, :subscribe, :sunion,
+    :sunionstore, :touch, :ttl, :type, :unsubscribe, :watch, :zadd,
+    :zcard, :zcount, :zincrby, :zinterstore, :zlexcount, :zrange,
+    :zrangebylex, :zrevrangebylex, :zrangebyscore, :zrank, :zrem,
+    :zremrangebylex, :zremrangebyrank, :zremrangebyscore, :zrevrange,
+    :zrevrangebyscore, :zrevrank, :zscore, :zunionstore, :sscan,
+    :hscan, :zscan]
 
   attr :redis
 
